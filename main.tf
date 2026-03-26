47f1eb36bfda332cf9dca811b75f9450dcaf7a4d
IshKevin <carterk279@gmail.com>
2026-03-26 16:59:10 +0200

    feat: Introduce variable usage and resource tagging
    - Parameterize VPC and subnet CIDR blocks
    - Use variables for EC2 instance type and SSH IP
    - Implement common tags for all resources

--- main.tf
+++ main.tf
@@ -1,73 +1,85 @@
-provider "aws" {
-    region = var.region
-}
-
-terraform {
-  backend "s3" {
-    bucket = "terraform20260326"
-    key = "terraform/state.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-lock"
-  }
-
-  }
-
-#vpc
+# VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
+
+  tags = merge(local.common_tags, {
+    Name = "main-vpc"
+  })
 }
 
-#subnet
+# Subnet
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, {
+    Name = "public-subnet"
+  })
 }
 
-#Internet Gateway
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "igw"
+  })
 }
 
-#Route Table
+# Route Table
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.main.id
-}
-  
-resource "aws_route" "route"{
- route_table_id = aws_route_table.rt.id
- destination_cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.igw.id
 
+  tags = merge(local.common_tags, {
+    Name = "public-rt"
+  })
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "rta" {
-  subnet_id = aws_subnet.public.id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.rt.id
 }
 
-#Security Group
+# Security Group
 resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.main.id
+
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = [var.my_ip]
   }
+
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
+  tags = merge(local.common_tags, {
+    Name = "web-sg"
+  })
+}
 
 # EC2 Instance
 resource "aws_instance" "ec2" {
-    ami = "ami-0c02fb55956c7d316"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.public.id
-    vpc_security_group_ids = [aws_security_group.sg.id]
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = merge(local.common_tags, {
+    Name = "web-server"
+  })
 }
