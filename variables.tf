5136794cedb53187c5b308ad667ba4123912001b
IshKevin <carterk279@gmail.com>
2026-03-26 16:52:22 +0200

    feat(variables): add core infrastructure vars
    - Introduce new VPC and subnet CIDR variables
    - Add S3 bucket and DynamoDB table variables
    - Include EC2 instance type variable
    - Remove

--- variables.tf
+++ variables.tf
@@ -1,8 +1,19 @@
-variable "region" {
-  default = "us-east-1"
+variable "region" {}
+variable "bucket_name" {}
+variable "dynamodb_table" {}
+variable "my_ip" {}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  default     = "10.0.0.0/16"
 }
 
-variable "my_ip" {
-    description = "102.22.146.226/32"
+variable "subnet_cidr" {
+  description = "Subnet CIDR"
+  default     = "10.0.1.0/24"
 }
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
